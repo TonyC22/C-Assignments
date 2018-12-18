2a1f269bf29a63c6ef0ed36c10eb8dbfdbecff99
TonyC22 <40578688+TonyC22@users.noreply.github.com>
2018-12-18 12:12:17 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int xFifty;
+            //int userInput;
+            //xFifty = 50;
+            //Console.WriteLine("Enter a number to be multiplied by 50.");
+            //userInput = int.Parse(Console.ReadLine());
+            //Console.WriteLine(+userInput * xFifty);
+            //Console.ReadLine();
+
+            //int addTwentyFive;
+            //int userInput;
+            //addTwentyFive = 25;
+            //Console.WriteLine("Enter a number to be added to 25.");
+            //userInput = int.Parse(Console.ReadLine());
+            //Console.WriteLine(+userInput + addTwentyFive);
+            //Console.ReadLine();
+
+            //decimal divideByTwelve;
+            //int userInput;
+            //divideByTwelve = 12.5m;
+            //Console.WriteLine("Enter a number to be divided by 12.5.");
+            //userInput = int.Parse(Console.ReadLine());
+            //Console.WriteLine(+userInput / divideByTwelve);
+            //Console.ReadLine();
+
+            //int greaterThanFifty = 50;
+            //int userInput;
+            //Console.WriteLine("Enter a number to see if it greater than 50.");
+            //userInput = int.Parse(Console.ReadLine());
+            //Console.WriteLine(+userInput >= greaterThanFifty);
+            //Console.ReadLine();
+
+            //int divideBySeven = 7;
+            //int userInput;
+            //Console.WriteLine("Enter a number to be divided by 7 and return the remainder.");
+            //userInput = int.Parse(Console.ReadLine());
+            //Console.WriteLine(+userInput % divideBySeven);
+            //Console.ReadLine();
+
+
+        }
+    }
+}
