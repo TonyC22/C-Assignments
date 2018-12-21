d6f862be6a80a0fdf646ac933e67d24f35c85fa1
TonyC22 <40578688+TonyC22@users.noreply.github.com>
2018-12-21 10:58:47 -0700

    Add files via upload

---
+++ While.cs
@@ -0,0 +1,35 @@
+﻿    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the coolest number between 20 and 30");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            bool correctNumber = false;
+
+
+            while (!correctNumber)
+            {
+                switch (userInput)
+                {
+                    case 23:
+                        Console.WriteLine("Nope try again");
+                        Console.WriteLine("What is the coolest number between 20 and 30");
+                        userInput = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 22:
+                        Console.WriteLine("Bingo!");
+                        correctNumber = true;
+                        break;
+                    default:
+                        Console.WriteLine("Nope try again");
+                        Console.WriteLine("What is the coolest number between 20 and 30");
+                        userInput = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WhileDo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the coolest number between 20 and 30");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            bool correctNumber = false;
+
+            do
+            {
+                switch (userInput)
+                {
+                    case 23:
+                        Console.WriteLine("Nope try again");
+                        Console.WriteLine("What is the coolest number between 20 and 30");
+                        userInput = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 22:
+                        Console.WriteLine("Bingo!");
+                        correctNumber = true;
+                        break;
+                    default:
+                        Console.WriteLine("Nope try again");
+                        Console.WriteLine("What is the coolest number between 20 and 30");
+                        userInput = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+
+            }
+            while (!correctNumber);
+           
+            Console.ReadLine();
+        }
+    }
+}
