27d13c57210991f45743b4a6f5d94dab31c49634
TonyC22 <40578688+TonyC22@users.noreply.github.com>
2019-01-08 12:36:45 -0700

    Upload

---
+++ Exercise124/Exercise124/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise124
+{
+    class Employee : Person
+    {
+        public new void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+
+        static void Main(string[] args)
+        {
+            Employee Employee = new Employee();
+            Employee.firstName = "Sample";
+            Employee.lastName = "Student";
+
+            Employee.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
