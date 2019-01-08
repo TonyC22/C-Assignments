01aa7ed63340630df03ce8dc6ab81cdd1c6b4312
TonyC22 <40578688+TonyC22@users.noreply.github.com>
2019-01-08 14:03:48 -0700

    Upload

---
+++ Exercise126/Exercise126/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise126
+{
+    class Employee : IQuittable
+    {
+        public void Quit()
+        {
+            Console.WriteLine("I quit!");
+        }
+
+        static void Main(string[] args)
+        {
+            IQuittable Employee = new Employee();
+            Employee.Quit();
+
+            Console.ReadLine();
+        }
+    }
+}
