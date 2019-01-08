fd59ca363953fd6dc16ce243aee3c2ef7554af10
TonyC22 <40578688+TonyC22@users.noreply.github.com>
2019-01-08 10:38:17 -0700

    Upload

---
+++ Exercise 120/Exercise 120/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_120
+{
+    public class Employee : Person
+    {
+        int Id;
+
+        static void Main(string[] args)
+        {
+            Employee Employee = new Employee();
+            Employee.FirstName = "Sample";
+            Employee.LastName = "Student";
+
+            Employee.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercise 120/Exercise 120/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_120
+{
+    public class Person
+    {
+        public String FirstName;
+        public String LastName;
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+
+    }
+}
+
