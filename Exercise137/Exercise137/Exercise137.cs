cbcf70aa1b4c7df14d1052d696dc7fd53fb2af93
TonyC22 <40578688+TonyC22@users.noreply.github.com>
2019-01-09 15:50:05 -0700

    Uploading Exercise

---
+++ Exercise137/Exercise137/Exercise137.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise137
+{
+    class Exercise137
+    {
+        static void Main(string[] args)
+        {
+            Number number;
+            number.Amount = 3.3m;
+
+            Console.WriteLine("The amount is: " + number.Amount);
+
+            Console.ReadLine();
+        }
+        struct Number
+        {
+            public decimal Amount;
+        }
+    }
+}
