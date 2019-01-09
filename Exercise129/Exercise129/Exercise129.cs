9c94e24f49f4141aadfe792dfeaa48569c231c2d
TonyC22 <40578688+TonyC22@users.noreply.github.com>
2019-01-08 17:24:49 -0700

    Upload

---
+++ Exercise129/Exercise129/Exercise129.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise129
+{
+
+    class Employee
+    {
+        int Id;
+
+        public static bool operator ==(Employee Emp1, Employee Emp2)
+        {
+            if (Emp1.Id == Emp2.Id)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public static bool operator !=(Employee Emp1, Employee Emp2)
+        {
+            if (Emp1.Id != Emp2.Id)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Employee Employee1 = new Employee();
+            Employee Employee2 = new Employee();
+
+            Employee1.Id = 2;
+            Employee2.Id = 2;
+
+            if (Employee1.Id == Employee2.Id)
+            {
+                Console.WriteLine("Equal");
+            }
+            else Console.WriteLine("Not equal");
+
+            Console.ReadLine();
+        }
+    }
+}
