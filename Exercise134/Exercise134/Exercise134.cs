27c11df2f44d674eb338a1e2b3b910ef604a52f4
TonyC22 <40578688+TonyC22@users.noreply.github.com>
2019-01-09 14:42:20 -0700

    Uploading Exercise

---
+++ Exercise134/Exercise134/Exercise134.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise134
+{
+    enum WeekDays
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    class Exercise134
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's the current day of the week?");
+            String input = Console.ReadLine();
+
+            try
+            {
+                WeekDays WeekDay = (WeekDays)Enum.Parse(typeof(WeekDays), input, true);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
