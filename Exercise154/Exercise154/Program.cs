966281e925827a175a527aeb79574c09875a1dbe
TonyC22 <40578688+TonyC22@users.noreply.github.com>
2019-01-10 19:32:29 -0700

    Uploading Exercise

---
+++ Exercise154/Exercise154/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exercise154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type a number.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\H39J61\Logs\log.txt", true))
+            {
+                file.WriteLine(userInput);
+            }
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\H39J61\Logs\log.txt");
+            System.Console.WriteLine("Contents of log.txt = ");
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.ReadLine();
+        }
+    }
+}
