cdca3709b441ca493e5b050713cf0c43ae0dcc35
TonyC22 <40578688+TonyC22@users.noreply.github.com>
2019-01-11 14:20:01 -0700

    Uploading Exercise

---
+++ Exercise156/Exercise156/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDate = DateTime.Now;
+            Console.WriteLine(currentDate);
+            Console.WriteLine("Please enter in a number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(currentDate.AddHours(userInput));
+            Console.ReadLine();
+        }
+    }
+}
