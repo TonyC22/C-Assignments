39f72747d85142b39f154db46e49900f1a7edfb0
TonyC22 <40578688+TonyC22@users.noreply.github.com>
2019-01-15 11:36:17 -0700

    Uploading Exercise 162

---
+++ Exercise162/Exercise162/AuctionCars.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise162
+{
+    public class AuctionCars
+    {
+        public AuctionCars(string CarTent) : this(CarTent, 1000)
+        {
+            return;
+        }
+        public AuctionCars(string carTent, int cars)
+        {
+            CarTent = carTent;
+            AllCars =  10000;
+        }
+        public string CarTent { get; set; }
+        public int AllCars { get; set; }
+    }
+}
---
+++ Exercise162/Exercise162/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise162
+{
+     public class Program
+    {
+        static void Main(string[] args)
+        {
+            const string auctionName = "Barret Jackson";
+
+            var numCars = "How many cars at Barret Jackson";
+
+            Console.WriteLine(auctionName, numCars);
+            Console.ReadLine();
+        }
+    }
+}
