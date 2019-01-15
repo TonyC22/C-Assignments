ad923fda295c7fa6cf32464c6e520f68a64268cf
TonyC22 <40578688+TonyC22@users.noreply.github.com>
2019-01-15 12:39:58 -0700

    Uploading Exercise

---
+++ Exercise165/Exercise165/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise165
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+            int userAge;
+                try
+                {
+                    Console.WriteLine("Please enter your age.");
+                    userAge = Convert.ToInt32(Console.ReadLine());
+                    if (userAge <= 0)
+                    {
+                        throw new InvalidAgeException(string.Format("Please enter your real age."));
+                    }
+
+                    DateTime userBirthDate = DateTime.Now.AddYears(-userAge);
+                    Console.WriteLine("You were born in " + userBirthDate.ToString("yyyy"));
+                    Console.ReadLine();
+                }
+                catch (InvalidAgeException)
+                {
+                    Console.WriteLine("Impossible age, Please enter your real age.");
+                    Console.ReadLine();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Whoops! There was an Error, please contact your Systems Administrator.");
+                    Console.ReadLine();
+                }
+            }
+    }
+}
